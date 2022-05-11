0779bb8ebcb75b603ef7447ea1c95e18608b8417
Victor Hugo Landin <victor.h.landin@gmail.com>
2022-05-11 20:54:27 -0300

    inicia o projeto

---
+++ src/main/java/com/zappts/MagicTheGathering/MagicTheGatheringApplication.java
@@ -0,0 +1,13 @@
+package com.zappts.MagicTheGathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicTheGatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicTheGatheringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zappts/MagicTheGathering/MagicTheGatheringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zappts.MagicTheGathering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicTheGatheringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
