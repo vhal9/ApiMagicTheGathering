9956350d3ed676856117aed6c05ae191735fdf36
Victor Hugo Landin <victor.h.landin@gmail.com>
2022-05-15 14:37:52 -0300

    adiciona anotaçao repository

--- src/main/java/com/zappts/MagicTheGathering/repository/CardRespository.java
+++ src/main/java/com/zappts/MagicTheGathering/repository/CardRespository.java
@@ -2,6 +2,8 @@ package com.zappts.MagicTheGathering.repository;
 
 import com.zappts.MagicTheGathering.domain.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CardRespository extends JpaRepository<Card, Long> {
 }
